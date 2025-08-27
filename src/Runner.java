33213ae2f777a80f6499aaa752640771883acd3c
Esteban Sánchez <estebansansan32@gmail.com>
2025-08-27 17:50:42 -0600

    Primer Versión Función Run
    Función run en PF, estática con manejo de excepciones.
     Nota: únicamente se  incluye la función Run, sin transpile ni build aun.

--- src/Runner.java
+++ src/Runner.java
@@ -1,6 +1,24 @@
 
+import java.util.function.Function;
+import java.util.function.Supplier;
+
 public class Runner {
-    public static void main(String[] args) {
-        System.out.println("RUN..." + System.getProperty("java.version") + "!");
+
+   
+    public static String run(String expressoFile) {
+        Function<String, String> toClassName =
+                file -> file.replace(".expresso", "");
+
+        Supplier<ProcessBuilder> processSupplier =
+                () -> new ProcessBuilder("java", toClassName.apply(expressoFile))
+                        .inheritIO();
+
+        try {
+            Process process = processSupplier.get().start();
+            process.waitFor();
+            return "Ejecución completada.";
+        } catch (Exception e) {
+            return "Error en ejecución: " + e.getMessage();
+        }
     }
 }
