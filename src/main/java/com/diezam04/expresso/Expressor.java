b2baa9d636723813136c90b1124e74cc09ee01c1
Josh Gámez Sánchez <j11gamez12@gmail.com>
2025-08-28 18:55:17 -0600

    Cambios importantes en Expressor.java. Implementación de la opción --out

--- src/main/java/com/diezam04/expresso/Expressor.java
+++ src/main/java/com/diezam04/expresso/Expressor.java
@@ -24,8 +24,12 @@ public class Expressor implements Runnable {
         CommandLine.usage(this, System.out);
     }
     
-    public Integer writeFile(java.io.File source,String fileContent, String extension) {  
-        String fileName = source.getName().split("\\.")[0]; 
+    public Integer writeFile(java.io.File source,String fileContent, String extension, java.io.File outDir) {  
+        String fileName = source.getName().split("\\.")[0];
+        if (!outDir.exists()) {
+            outDir.mkdirs(); 
+        }
+        java.io.File outFile = new java.io.File(outDir, fileName + "." + extension); 
         try (FileWriter writer = new FileWriter(fileName + "." + extension)) {
             writer.write(fileContent);
             System.out.println("File written successfully");
@@ -45,19 +49,27 @@ public class Expressor implements Runnable {
         }
     }
 
-    @Command(name = "transpile", description = "Transpile a source file from expressor to java")
-    public Integer transpile(@Parameters(index = "0", paramLabel = "SOURCE",
-                description = "The source file to transpile")
-    java.io.File source) {
-        return writeFile(source,Transpiler.run( loadFile(source) ),"java");
-    }
+    @Command(name = "transpile", description = "Transpile a source file from expresso to java")
+    public Integer transpile(
+        @Parameters(index = "0", paramLabel = "SOURCE", description = "The source file to transpile")
+        java.io.File source,
+        @Option(names = {"--out"}, description = "Directorio de salida", defaultValue = ".")
+        java.io.File outDir
+    ) {
+    return writeFile(source, Transpiler.run(loadFile(source)), "java", outDir);
+   }
+
 
     @Command(name = "build", description = "Build a source file, it generates a .class")
-    public Integer build(@Parameters(index = "0", paramLabel = "SOURCE",
-                description = "The source file to build")
-    java.io.File source) {
-        return writeFile(source,Builder.run(Transpiler.run(loadFile(source))),"class");
+    public Integer build(
+        @Parameters(index = "0", paramLabel = "SOURCE", description = "The source file to build")
+        java.io.File source,
+        @Option(names = {"--out"}, description = "Directorio de salida", defaultValue = ".")
+        java.io.File outDir
+    ) {
+    return writeFile(source, Builder.run(Transpiler.run(loadFile(source))), "class", outDir);
     }
+
 
     @Command(name = "run", description = "Run a source file, executes .class files")
     public Integer run(@Parameters(index = "0", paramLabel = "SOURCE",
