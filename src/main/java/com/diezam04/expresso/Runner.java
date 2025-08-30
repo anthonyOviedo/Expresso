c372ff3ee343796035c77f52d7f4e07ea3d44955
Esteban Sánchez <estebansansan32@gmail.com>
2025-08-30 00:17:37 -0600

    Full-Logs
    Se sustituyen los System.out.println(), por la función log().

--- src/main/java/com/diezam04/expresso/Runner.java
+++ src/main/java/com/diezam04/expresso/Runner.java
@@ -1,29 +1,33 @@
 package com.diezam04.expresso;
 
-
 import java.io.File;
 import java.util.function.Function;
 
 public class Runner {
-    
+
     public static String run(File classFile) {
         Function<String, String> toClassName =
                 file -> file.replace(".class", "");
-        
+
         try {
             String className = toClassName.apply(classFile.getName());
-            System.out.println("Executing class: " + className);
+            log("Executing class: " + className);
             ProcessBuilder processBuilder = new ProcessBuilder("java", className);
-            processBuilder.inheritIO(); 
+            processBuilder.inheritIO();
             Process process = processBuilder.start();
             process.waitFor();
             return "Execution completed.";
         } catch (Exception e) {
-            return "Error. Execution not completed." + e.getMessage();
+            return "Error. Execution not completed. " + e.getMessage();
         }
     }
+
     public static Integer run(String classFile) {
-        System.out.println("Executing class: " + classFile);
+        log("Executing class: " + classFile);
         return 0;
+    }
+
+    private static void log(String message) {
+        System.out.println(message);
     }
 }
